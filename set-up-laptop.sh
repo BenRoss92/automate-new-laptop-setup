d2eaa4c24116349235d891c417e9566264b3cb6f
Ben Ross <benjamin.ross@youview.com>
2023-06-08 15:55:41 +0100

    Bash script: check Xcode has been installed

--- set-up-laptop.sh
+++ set-up-laptop.sh
@@ -3,5 +3,8 @@
 # Install a fresh version of Homebrew without prompting the user - see https://github.com/Homebrew/install#:~:text=If%20you%20want%20to%20run%20the%20Homebrew%20installer%20non%2Dinteractively%20without%20prompting%20for%20passwords%20(e.g.%20in%20automation%20scripts)%2C%20you%20can%20use%3A
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Check fresh Homebrew version is installed and available on the MacOS Virtual Machine
+# Check Homebrew has been installed
 brew help
+
+# Check Xcode Command Line Tools have been installed by Homebrew
+xcode-select -p
