f1634ecad3e314ab470aa387e515568090cd5897
Ben Ross <benjamin.ross@youview.com>
2023-08-03 17:48:35 +0100

    Fail bash script immediately on first error
    - When there is an error, no subsequent commands inside of the bash script will be run. The script will immediately exit with an error code.

--- set-up-laptop.sh
+++ set-up-laptop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+# If any command fails, stop executing subsequent commands and fail the script
+set -e
+
 # Install a fresh version of Homebrew without prompting the user - see https://github.com/Homebrew/install#:~:text=If%20you%20want%20to%20run%20the%20Homebrew%20installer%20non%2Dinteractively%20without%20prompting%20for%20passwords%20(e.g.%20in%20automation%20scripts)%2C%20you%20can%20use%3A
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
